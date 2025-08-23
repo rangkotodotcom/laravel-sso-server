93bb6023075bf5e15ca674ef600a03b74a027911
jamilur <jamilur.nutech@gmail.com>
2025-08-23 11:42:43 +0700

    update

--- database/migrations/0001_01_01_000000_create_user_groups_table.php
+++ database/migrations/0001_01_01_000000_create_user_groups_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('created_by');
             $table->string('updated_by')->nullable();
             $table->string('deleted_by')->nullable();
-            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
